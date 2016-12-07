75d5bcc8f3f52d8a055d494eaf0bf51e9e414757
Juan Puelpan <jpuelpan528@gmail.com>
2016-12-06 21:56:39 -0300

    More testing

--- spec/abstract_api_wrapper_spec.rb
+++ spec/abstract_api_wrapper_spec.rb
@@ -12,7 +12,6 @@ describe AbstractApiWrapper do
   describe 'Client' do
     it 'should create a client with the given options' do
       client = AbstractApiWrapper::Client.new(TEST_OPTIONS)
-
       expect(client.access_token).to eq('mysupersecrettoken')
       expect(client.base_url).to eq('https://mysuperapi.com/api')
       expect(client.apiver).to eq('v3')
@@ -20,14 +19,23 @@ describe AbstractApiWrapper do
   end
 
   describe 'Request' do
-
     before(:each) do
       @client = AbstractApiWrapper::Client.new(TEST_OPTIONS)
     end
 
     it 'should return a request instance' do
       request = @client.users.all
-      expect(request).to be_kind_of(AbstractApiWrapper::Request)
+      expect(request.client).to be_kind_of(AbstractApiWrapper::Request)
+    end
+
+    it 'should return the same request instance' do
+      user = @client.users.find(1)
+      project = user.projects.find(1)
+
+      expect(user).to be_kind_of(AbstractApiWrapper::Request)
+      expect(project).to be_kind_of(AbstractApiWrapper::Request)
+
+      expect(user.object_id).to eq(project.object_id)
     end
 
     it 'should create a path as array' do
@@ -41,6 +49,12 @@ describe AbstractApiWrapper do
       request = @client.users.all(active: true)
       expect(request.filters).to be_kind_of(Hashie::Mash)
       expect(request.filters).to eq({ 'active' => true })
+    end
+
+    it 'should set the path chain' do
+      request = @client.users.find(1).projects.all(active: true)
+      expect(request.chain).to be_kind_of(Array)
+      expect(request.chain).to eq(['users', 'find', 'projects', 'all'])
     end
   end
 
