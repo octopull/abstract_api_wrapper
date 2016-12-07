32cefa92c0fa2fe3be3e62745824469d18e275e3
Juan Puelpan <jpuelpan528@gmail.com>
2016-12-07 03:22:25 -0300

    Added webmock for requests testing

--- spec/abstract_api_wrapper_spec.rb
+++ spec/abstract_api_wrapper_spec.rb
@@ -1,16 +1,19 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
+require 'webmock/rspec'
 require 'abstract_api_wrapper'
 
-describe AbstractApiWrapper do
-  TEST_OPTIONS = {
-    base_url: 'https://mysuperapi.com/api',
-    apiver: 'v3',
-    access_token: 'mysupersecrettoken'
-  }
+include WebMock::API
+WebMock.enable!
 
+TEST_OPTIONS = {
+  base_url: 'https://mysuperapi.com/api',
+  apiver: 'v3',
+  access_token: 'mysupersecrettoken'
+}
+
+describe AbstractApiWrapper do
   describe 'Client' do
-    it 'should create a client with the given options' do
+    it 'should create a Client with the given options' do
       client = AbstractApiWrapper::Client.new(TEST_OPTIONS)
       expect(client.access_token).to eq('mysupersecrettoken')
       expect(client.base_url).to eq('https://mysuperapi.com/api')
@@ -23,12 +26,12 @@ describe AbstractApiWrapper do
       @client = AbstractApiWrapper::Client.new(TEST_OPTIONS)
     end
 
-    it 'should return a request instance' do
+    it 'should return a Request instance' do
       request = @client.users.all
       expect(request.client).to be_kind_of(AbstractApiWrapper::Request)
     end
 
-    it 'should return the same request instance' do
+    it 'should return the same Request instance' do
       user = @client.users.find(1)
       project = user.projects.find(1)
 
@@ -56,6 +59,55 @@ describe AbstractApiWrapper do
       expect(request.chain).to be_kind_of(Array)
       expect(request.chain).to eq(['users', 'find', 'projects', 'all'])
     end
+  end
+
+  describe 'Response' do
+    before(:each) do
+      @client = AbstractApiWrapper::Client.new(TEST_OPTIONS)
+
+      stub_request(:get, TEST_OPTIONS[:base_url])
+        .to_return(:status => 200, :body => '[]')
+
+      @request = Faraday.get(TEST_OPTIONS[:base_url])
+    end
+
+    it 'should return a Response instance' do
+      response = AbstractApiWrapper::Response.new(@request)
+      expect(response).to be_kind_of(AbstractApiWrapper::Response)
+    end
+
+    it 'should parse the response body as JSON' do
+      response = AbstractApiWrapper::Response.new(@request)
+      expect(response.body).to be_kind_of(AbstractApiWrapper::Response::Collection)
+    end
+
+    describe 'Resource' do
+      before(:each) do
+        @item = { id: 1, full_name: 'Jon Snow', email: 'jon@winterfell.com' }
+      end
+
+      it 'should create a new Resource from a Hash' do
+        resource = AbstractApiWrapper::Response::Resource.new(@item)
+
+        expect(resource).to be_kind_of(Hashie::Mash)
+        expect(resource.id).to eq(1)
+        expect(resource.full_name).to eq('Jon Snow')
+        expect(resource.email).to eq('jon@winterfell.com')
+        expect(resource.request).to be_nil
+      end
+
+      it 'should assign the Request object from params' do
+        request = Faraday.get(TEST_OPTIONS[:base_url])
+        resource = AbstractApiWrapper::Response::Resource.new(@item, request)
+
+        expect(resource).to be_kind_of(Hashie::Mash)
+        expect(resource.request).to be_kind_of(Faraday::Response)
+      end
+    end
+
+    # TODO: Collection tests
+    # describe 'Collection' do
+    # end
   end
 
 end
